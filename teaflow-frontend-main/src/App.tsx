e43607e2d1fd758a17df053fba75e9e9c4152c75
Cong Tam Tran <161344721+Thormastran@users.noreply.github.com>
2026-01-13 15:28:13 +0700

    feature: fit configure

--- teaflow-frontend-main/src/App.tsx
+++ teaflow-frontend-main/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import "./App.css"
+import { Header } from "./components/ui/header-2"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full">
+			<Header />
+
+			<main className="mx-auto min-h-screen w-full max-w-3xl px-4 py-12">
+				<div  className="space-y-2 mb-4">
+					<div className="bg-accent h-6 w-4/6 rounded-md border" />
+					<div className="bg-accent h-6 w-1/2 rounded-md border" />
+				</div>
+				<div  className="flex gap-2 mb-8">
+					<div className="bg-accent h-3 w-14 rounded-md border" />
+					<div className="bg-accent h-3 w-12 rounded-md border" />
+				</div>
+
+				{Array.from({ length: 7 }).map((_, i) => (
+					<div key={i} className="space-y-2 mb-8">
+						<div className="bg-accent h-4 w-full rounded-md border" />
+						<div className="bg-accent h-4 w-full rounded-md border" />
+						<div className="bg-accent h-4 w-full rounded-md border" />
+						<div className="bg-accent h-4 w-1/2 rounded-md border" />
+					</div>
+				))}
+			</main>
+		</div>
   )
 }
 
